ae62db2f6fe0c52c84dfcbd02ecf4376b66d5c2e
hayato uchimura <uhayato@hayatonoMacBook-Air.local>
2025-10-01 23:33:19 +0900

    macからのコミット

---
+++ name.py
@@ -0,0 +1,20 @@
+#print('aを入力')
+#a = input()
+#rc = a.startswith("Hello")
+#if(rc == True):
+#    {
+#        print("文字はHelloから始まります")
+#    }
+#else:
+#    {
+#        print("文字はHelloから始まりません")
+#    }
+
+a = [1,2,3]
+
+b = [5,6,7]
+
+x = [a,b]
+
+a.reverse()
+print(a)
